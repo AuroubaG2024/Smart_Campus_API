4e3cbbe4865fdbd084c0ab7966fc6a77e4bc0bc7
aurou <aurou@LAPTOP-1D45J517.communityfibre.co.uk>
2026-04-25 14:25:53 +0100

    Updated the files and added the project onto Github as they have been modified

--- src/main/java/com/smartcampus/resource/DiscoveryResource.java
+++ src/main/java/com/smartcampus/resource/DiscoveryResource.java
@@ -11,18 +11,24 @@ public class DiscoveryResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Map<String, Object> getApiInfo() {
+        //Creates a map to store information about the API
         Map<String, Object> info = new HashMap<>();
-
+       
+        
         info.put("name", "Smart Campus API");
         info.put("version", "v1");
+        //Provides the administrator contact email for the API
         info.put("adminContact", "admin@westminster.ac.uk");
-
+        //Creates another map to store the available API resource endpoints
         Map<String, String> resources = new HashMap<>();
+        //Defines the endpoints for rooms
         resources.put("rooms", "/api/v1/rooms");
+        //Defines the endpoints for the sensors
         resources.put("sensors", "/api/v1/sensors");
-
+        
+        //Add the resouces map into the API response
         info.put("resources", resources);
-
+        //Returns the API information as a JSON object 
         return info;
     }
 }
