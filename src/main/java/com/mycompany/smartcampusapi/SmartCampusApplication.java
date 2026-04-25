254959c8f7a2e44bafcbbe3a947f08d60eedd4f5
aurou <aurou@LAPTOP-1D45J517.communityfibre.co.uk>
2026-04-25 14:25:27 +0100

    Updated the files and added the project onto Github as they have been modified

--- src/main/java/com/mycompany/smartcampusapi/SmartCampusApplication.java
+++ src/main/java/com/mycompany/smartcampusapi/SmartCampusApplication.java
@@ -1,8 +1,15 @@
+/*
+Name:Aurouba Gillani
+ID number: w20456331
+Course Module: 5COSC022W Client-Server Architectures
+*/
+
+
 package com.mycompany.smartcampusapi;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/api/v1")
+@ApplicationPath("/api/v1") //Uses the path for the server to run
 public class SmartCampusApplication extends Application {
 }
