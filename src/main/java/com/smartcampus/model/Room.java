d1df0306dc11078fffedad90af7d0c3211ac28a2
aurou <aurou@LAPTOP-1D45J517.communityfibre.co.uk>
2026-04-25 14:26:15 +0100

    Updated the files and added the project onto Github as they have been modified

--- src/main/java/com/smartcampus/model/Room.java
+++ src/main/java/com/smartcampus/model/Room.java
@@ -1,20 +1,19 @@
 package com.smartcampus.model;
 
-
 import java.util.List;
 
 public class Room {
-
+    //Defining variables 
     private String id;
     private String name;
     private int capacity;
     private List<String> sensorIds;
 
-    // empty constructor (needed for JSON)
+    //empty constructor which is needed for JSON postman tests
     public Room() {
     }
 
-    // full constructor
+    //Constructor for all values
     public Room(String id, String name, int capacity, List<String> sensorIds) {
         this.id = id;
         this.name = name;
@@ -22,34 +21,34 @@ public class Room {
         this.sensorIds = sensorIds;
     }
 
-    // getters
+    //Getters and Setters
     public String getId() {
         return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public List<String> getSensorIds() {
-        return sensorIds;
-    }
-
-    // setters
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
 
+    
+    public int getCapacity() {
+        return capacity;
+    }
+    
     public void setCapacity(int capacity) {
         this.capacity = capacity;
+    }
+
+    public List<String> getSensorIds() {
+        return sensorIds;
     }
 
     public void setSensorIds(List<String> sensorIds) {
