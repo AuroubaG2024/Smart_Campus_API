08b33cdfb510d4e5aadc55bebc22c4091755bf50
aurou <aurou@LAPTOP-1D45J517.communityfibre.co.uk>
2026-04-25 14:26:08 +0100

    Updated the files and added the project onto Github as they have been modified

--- src/main/java/com/smartcampus/resource/SensorReadingResource.java
+++ src/main/java/com/smartcampus/resource/SensorReadingResource.java
@@ -1,10 +1,11 @@
 package com.smartcampus.resource;
 import com.smartcampus.exception.LinkedResourceNotFoundException;
 import com.smartcampus.exception.SensorUnavailableException;
+//Imports the model.classes representing sensors and their readings
 import com.smartcampus.model.Sensor;
 import com.smartcampus.model.SensorReading;
 import com.smartcampus.store.DataStore;
-
+//JAX-RS imports for REST API annotations and response handling
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,11 +13,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+//Defines the base URI for this resource.
+//This represents a nested endpoint for sensor readings
 @Path("/sensors/{sensorId}/readings")
+//API produces JSON responses 
 @Produces(MediaType.APPLICATION_JSON)
+//API accepts JSON input
 @Consumes(MediaType.APPLICATION_JSON)
 public class SensorReadingResource {
     
+    //Handles HTTP GET requests to retrieve all readings for a specific sensor
     @GET
     public List<SensorReading> getReadings(@PathParam("sensorId") String sensorId) {
 
@@ -30,7 +36,8 @@ public class SensorReadingResource {
 
         return DataStore.readings.getOrDefault(sensorId, new ArrayList<>());
     }
-
+    
+    //Handles HTTP POST requests to add a new reading for a sensor
     @POST
     public Response addReading(@PathParam("sensorId") String sensorId, SensorReading reading) {
 
@@ -53,13 +60,15 @@ public class SensorReadingResource {
         reading.setTimestamp(System.currentTimeMillis());
 
         //add reading to datastore
+        //computeIfAbsent ensures a list exists for the sensor ID
         DataStore.readings
                 .computeIfAbsent(sensorId, k -> new ArrayList<>())
                 .add(reading);
 
         //update the sensor's current value 
         sensor.setCurrentValue(reading.getValue());
-
+        
+        //Returns the HTTP status 201 (Created) with the newly created reading
         return Response.status(Response.Status.CREATED)
                 .entity(reading)
                 .build();
