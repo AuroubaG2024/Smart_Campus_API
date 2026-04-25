4892727a14b80639590c608374646f5cec514d16
aurou <aurou@LAPTOP-1D45J517.communityfibre.co.uk>
2026-04-25 14:25:01 +0100

    Updated the files and added the project onto Github as they have been modified

--- src/main/java/com/smartcampus/exception/RoomNotEmptyException.java
+++ src/main/java/com/smartcampus/exception/RoomNotEmptyException.java
@@ -1,6 +1,7 @@
 package com.smartcampus.exception;
 
 public class RoomNotEmptyException extends RuntimeException {
+    //A Constructor is used to allow an error message to be passed when the exception is thrown
     public RoomNotEmptyException(String message) {
         super(message);
     }
