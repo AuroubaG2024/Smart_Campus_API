0b6dd9f8bb6ab617a23bc8ac46f365eb4622f71f
aurou <aurou@LAPTOP-1D45J517.communityfibre.co.uk>
2026-04-25 14:24:54 +0100

    Updated the files and added the project onto Github as they have been modified

--- src/main/java/com/smartcampus/mapper/GlobalExceptionMapper.java
+++ src/main/java/com/smartcampus/mapper/GlobalExceptionMapper.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.smartcampus.mapper;
 
-/**
- *
- * @author aurou
- */
-public class GlobalExceptionMapper {
-    
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.core.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//This mapper catches all unhandled exceptions in the SmartCampus API.
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
+
+    @Override
+    public Response toResponse(Throwable exception) {
+
+        Map<String, Object> error = new HashMap<>();
+        //returns a structured JSON response instead
+        error.put("message", "An unexpected server error occurred.");
+        error.put("status", 500);
+        error.put("error", exception.getMessage());
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity(error)
+                .build();
+    }
 }
