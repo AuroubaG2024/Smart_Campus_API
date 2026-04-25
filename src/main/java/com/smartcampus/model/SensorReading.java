c705aaea7940e486a7c53988a18747fe90895ccc
aurou <aurou@LAPTOP-1D45J517.communityfibre.co.uk>
2026-04-25 14:24:41 +0100

    Updated the files and added the project onto Github as they have been modified

--- src/main/java/com/smartcampus/model/SensorReading.java
+++ src/main/java/com/smartcampus/model/SensorReading.java
@@ -1,26 +1,46 @@
 package com.smartcampus.model;
 
 public class SensorReading {
-
+    
+    //Defining variables
     private String id;
     private long timestamp;
     private double value;
-
+  
+    //Empty constructor for the JSON body for the postman tests 
     public SensorReading() {}
-
+  
+    //Constructor for all the values
     public SensorReading(String id, long timestamp, double value) {
         this.id = id;
         this.timestamp = timestamp;
         this.value = value;
     }
+    
+    
+    //Getters and Setters
+    public String getId() { 
+        return id; 
+    }
+    
+    public void setId(String id) {
+        this.id = id; 
+    }
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+    public long getTimestamp() { 
+        return timestamp; 
+    }
+    
+    public void setTimestamp(long timestamp) { 
+        this.timestamp = timestamp; 
+    }
 
-    public long getTimestamp() { return timestamp; }
-    public void setTimestamp(long timestamp) { this.timestamp = timestamp; }
-
-    public double getValue() { return value; }
-    public void setValue(double value) { this.value = value; }
+    public double getValue() { 
+        return value; 
+    }
+    
+    public void setValue(double value) { 
+        this.value = value; 
+    }
     
 }
