fd44c2083b84041a405f15bfdda2d0114b0e54f5
aurou <aurou@LAPTOP-1D45J517.communityfibre.co.uk>
2026-04-25 14:25:41 +0100

    Updated the files and added the project onto Github as they have been modified

--- src/main/java/com/smartcampus/exception/LinkedResourceNotFoundException.java
+++ src/main/java/com/smartcampus/exception/LinkedResourceNotFoundException.java
@@ -1,8 +1,7 @@
 package com.smartcampus.exception;
 
-
 public class LinkedResourceNotFoundException extends RuntimeException{
-    
+    //A Constructor is used to allow an error message to be passed when the exception is thrown
     public LinkedResourceNotFoundException(String message) {
         super(message);
     }
