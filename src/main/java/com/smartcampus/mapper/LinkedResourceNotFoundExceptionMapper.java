9de47df58eafba7a4563342dc32aaea4c5de104a
aurou <aurou@LAPTOP-1D45J517.communityfibre.co.uk>
2026-04-25 14:25:47 +0100

    Updated the files and added the project onto Github as they have been modified

--- src/main/java/com/smartcampus/mapper/LinkedResourceNotFoundExceptionMapper.java
+++ src/main/java/com/smartcampus/mapper/LinkedResourceNotFoundExceptionMapper.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.smartcampus.mapper;
 
-/**
- *
- * @author aurou
- */
-public class LinkedResourceNotFoundExceptionMapper {
-    
+import com.smartcampus.exception.LinkedResourceNotFoundException;
+
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.core.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Instead of returning a server error, the API returns a structured response of HTTP 422
+
+@Provider
+public class LinkedResourceNotFoundExceptionMapper implements ExceptionMapper<LinkedResourceNotFoundException> {
+    // mapper handles cases where a referenced resource does not exist
+    @Override
+    public Response toResponse(LinkedResourceNotFoundException exception) {
+
+        Map<String, Object> error = new HashMap<>();
+        //the API returns a structured HTTP 422 Unprocessable Entity response
+        error.put("message", exception.getMessage());
+        error.put("status", 422);
+        error.put("error", "Linked resource not found");
+
+        return Response.status(422)
+                .entity(error)
+                .build();
+    }
 }
