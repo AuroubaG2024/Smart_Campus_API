039d6bdbf23ce5589cb3e5ff7d4c9d9b72ffac16
aurou <aurou@LAPTOP-1D45J517.communityfibre.co.uk>
2026-04-25 14:25:21 +0100

    Updated the files and added the project onto Github as they have been modified

--- src/main/java/com/smartcampus/mapper/SensorUnavailableExceptionMapper.java
+++ src/main/java/com/smartcampus/mapper/SensorUnavailableExceptionMapper.java
@@ -4,9 +4,10 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-@Provider
+@Provider //This annotation registers the mapper with JAX-RS
 public class SensorUnavailableExceptionMapper implements ExceptionMapper<SensorUnavailableException> {
-
+    
+    //Deplays the response status
     @Override
     public Response toResponse(SensorUnavailableException ex) {
 
